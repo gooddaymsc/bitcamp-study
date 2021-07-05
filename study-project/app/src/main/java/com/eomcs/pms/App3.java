b10896f6aeafbacb2c21f9843d32d37e7fa0807b
gooddaymsc@naver.com <gooddaymsc@naver.com>
2021-07-05 18:11:10 +0900

    ok

---
+++ study-project/app/src/main/java/com/eomcs/pms/App3.java
@@ -0,0 +1,13 @@
+package com.eomcs.pms;
+
+public class App3 {
+  public static void main(String[] args) {
+    System.out.printf("[%s]\n", "작업");
+    System.out.printf("프로젝트: %s\n", "미니 프로젝트 관리 시스템");
+    System.out.printf("번호: %d\n", 1);
+    System.out.println("내용: 요구사항 수집");
+    System.out.println("완료일: 2020-01-02");
+    System.out.println("상태: 진행중");
+    System.out.println("담당자: 홍길동");
+  }
+}
