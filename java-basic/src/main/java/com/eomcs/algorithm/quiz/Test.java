36841fd9a657444f3bf335b6a7d4e4bdf797a0e6
unknown <gooddaymsc@naver.com>
2021-07-16 21:27:45 +0900

    ..

--- java-basic/src/main/java/com/eomcs/algorithm/quiz/Test.java
+++ java-basic/src/main/java/com/eomcs/algorithm/quiz/Test.java
@@ -36,22 +36,17 @@ public class Test {
   static boolean isGeometricProgression(int[] sequence) {
 
     for (int i = 1; i < sequence.length; i++) {
-      if (sequence[i] ) {
+      if (sequence[i] * sequence[0] !=  sequence[i - 1] * sequence[1]) {
         return false;
       }
     }
-
-
-
+    return true;
   }
-
 
   public static void main(String[] args) {
     System.out.println(isGeometricProgression(new int[]{1,4,16}));
     System.out.println(isGeometricProgression(new int[]{2,4,8,17,34}));
-
   }
-
 }
 
 
