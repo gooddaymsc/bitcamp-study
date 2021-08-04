8c3f8fbc612b7af1c4fe4a9ca05cb5419604f66d
gooddaymsc@naver.com <gooddaymsc@naver.com>
2021-08-04 21:03:16 +0900

    ..

--- study-project/app/src/main/java/com/eomcs/pms/App.java
+++ study-project/app/src/main/java/com/eomcs/pms/App.java
@@ -20,7 +20,9 @@ public class App {
     app.service();
   }
 
-  public void service() {
+  void service() {
+    //    Menu mainMenu = createMenu();
+    //    mainMenu.execute();
     createMenu().execute();
     Prompt.close();
   }
@@ -33,144 +35,100 @@ public class App {
     mainMenuGroup.add(boardMenu);
 
     boardMenu.add(new Menu("등록") {
-      @Override
       public void execute() {
-        boardHandler.add();
-      }
-    });
-
+        boardHandler.add(); 
+      }});
     boardMenu.add(new Menu("목록") {
-      @Override
       public void execute() {
-        boardHandler.list();
-      }
-    });
+        boardHandler.list(); 
+      }});
     boardMenu.add(new Menu("상세보기") {
-      @Override
       public void execute() {
-        boardHandler.detail();
-      }
-    });
+        boardHandler.detail(); 
+      }});
     boardMenu.add(new Menu("변경") {
-      @Override
       public void execute() {
-        boardHandler.update();
-      }
-    });
+        boardHandler.update(); 
+      }});
     boardMenu.add(new Menu("삭제") {
-      @Override
       public void execute() {
-        boardHandler.delete();
-      }
-    });
+        boardHandler.delete(); 
+      }});
 
     MenuGroup memberMenu = new MenuGroup("회원");
     mainMenuGroup.add(memberMenu);
 
     memberMenu.add(new Menu("등록") {
-      @Override
       public void execute() {
-        memberHandler.add();
-      }
-    });
+        memberHandler.add(); 
+      }});
     memberMenu.add(new Menu("목록") {
-      @Override
       public void execute() {
-        memberHandler.list();
-      }
-    });
+        memberHandler.list(); 
+      }});
     memberMenu.add(new Menu("상세보기") {
-      @Override
       public void execute() {
-        memberHandler.detail();
-      }
-    });
+        memberHandler.detail(); 
+      }});
     memberMenu.add(new Menu("변경") {
-      @Override
       public void execute() {
-        memberHandler.update();
-      }
-    });
+        memberHandler.update(); 
+      }});
     memberMenu.add(new Menu("삭제") {
-      @Override
       public void execute() {
-        memberHandler.delete();
-      }
-    });
+        memberHandler.delete(); 
+      }});
 
     MenuGroup projectMenu = new MenuGroup("프로젝트");
     mainMenuGroup.add(projectMenu);
 
     projectMenu.add(new Menu("등록") {
-      @Override
       public void execute() {
-        projectHandler.add();
-      }
-    });
+        projectHandler.add(); 
+      }});
     projectMenu.add(new Menu("목록") {
-      @Override
       public void execute() {
-        projectHandler.list();
-      }
-    });
+        projectHandler.list(); 
+      }});
     projectMenu.add(new Menu("상세보기") {
-      @Override
       public void execute() {
-        projectHandler.detail();
-      }
-    });
+        projectHandler.detail(); 
+      }});
     projectMenu.add(new Menu("변경") {
-      @Override
       public void execute() {
-        projectHandler.update();
-      }
-    });
+        projectHandler.update(); 
+      }});
     projectMenu.add(new Menu("삭제") {
-      @Override
       public void execute() {
-        projectHandler.delete();
-      }
-    });
+        projectHandler.delete(); 
+      }});
 
     MenuGroup taskMenu = new MenuGroup("작업");
     mainMenuGroup.add(taskMenu);
 
     taskMenu.add(new Menu("등록") {
-      @Override
       public void execute() {
-        taskHandler.add();
-      }
-    });
+        taskHandler.add(); 
+      }});
     taskMenu.add(new Menu("목록") {
-      @Override
       public void execute() {
-        taskHandler.list();
-      }
-    });
+        taskHandler.list(); 
+      }});
     taskMenu.add(new Menu("상세보기") {
-      @Override
       public void execute() {
-        taskHandler.detail();
-      }
-    });
+        taskHandler.detail(); 
+      }});
     taskMenu.add(new Menu("변경") {
-      @Override
       public void execute() {
-        taskHandler.update();
-      }
-    });
+        taskHandler.update(); 
+      }});
     taskMenu.add(new Menu("삭제") {
-      @Override
       public void execute() {
-        taskHandler.delete();
-      }
-    });
+        taskHandler.delete(); 
+      }});
 
     return mainMenuGroup;
-
   }
-
-
 }
 
 
--- study-project/app/src/main/java/com/eomcs/pms/handler/TaskHandler.java
+++ study-project/app/src/main/java/com/eomcs/pms/handler/TaskHandler.java
@@ -11,23 +11,24 @@ public class TaskHandler {
   Task[] tasks = new Task[MAX_LENGTH];
   int size = 0;
 
-  // 이제 의존 객체는 생성자를 통해 주입 받기 때문에 
-  // 외부에서 인스턴스 변수에 직접 접근할 이유가 없다.
-  // 따라서 전체 공개 모드에서 패키지 멤버에게만 공개하는 모드로 전환한다. 
   MemberHandler memberHandler;
 
-
-  // TaskHandler의 의존 객체를 반드시 주입하도록 강제하고 싶다면,
-  // 생성자를 선언할 때 파라미터로 지정하라.
-  // 즉 TaskHandler의 인스턴스를 생성할 때 필요한 값이 있다면,
-  // 생성자의 파라미터를 이용해서 받을 수 있다.
   public TaskHandler(MemberHandler memberHandler) {
     this.memberHandler = memberHandler;
   }
 
+  static class Node {
+    Task task;
+    Node next;
 
-  // add()에서 사용할 MemberHandler는 메서드를 호출하기 전에 
-  // 인스턴스 변수에 미리 주입되어 있어야 한다.
+    public Node (Task task) {
+      this.task = task;
+    }
+  }
+
+  Node head;
+  Node tail;
+
   public void add() {
     System.out.println("[작업 등록]");
 
@@ -43,21 +44,35 @@ public class TaskHandler {
       return; 
     }
 
-    this.tasks[this.size++] = task;
+    Node node = new Node(task);
+
+    if (head == null) {
+      tail = head = node;
+    } else {
+      tail.next = node;
+      tail = node;
+    }
+    size++;
   }
 
-  //다른 패키지에 있는 App 클래스가 다음 메서드를 호출할 수 있도록 공개한다.
   public void list() {
     System.out.println("[작업 목록]");
 
-    for (int i = 0; i < this.size; i++) {
-      System.out.printf("%d, %s, %s, %s, %s\n",
-          this.tasks[i].no, 
-          this.tasks[i].content, 
-          this.tasks[i].deadline, 
-          getStatusLabel(this.tasks[i].status), 
-          this.tasks[i].owner);
+    if (head == null) {
+      return;
     }
+
+    Node node = head;
+
+    do {
+      System.out.printf("%d, %s, %s, %s, %s\n",
+          node.task.no, 
+          node.task.content, 
+          node.task.deadline, 
+          getStatusLabel(node.task.status), 
+          node.task.owner);
+      node = node.next;
+    } while (node != null);
   }
 
   public void detail() {
@@ -76,8 +91,6 @@ public class TaskHandler {
     System.out.printf("담당자: %s\n", task.owner);
   }
 
-  // update()가 사용할 MemberHandler 는 
-  // 인스턴스 변수에 미리 주입 받기 때문에 파라미터로 받을 필요가 없다.
   public void update() {
     System.out.println("[작업 변경]");
     int no = Prompt.inputInt("번호? ");
@@ -116,8 +129,9 @@ public class TaskHandler {
     System.out.println("[작업 삭제]");
     int no = Prompt.inputInt("번호? ");
 
-    int index = indexOf(no);
-    if (index == -1) {
+    Task task = findByNo(no);
+
+    if (task == null) {
       System.out.println("해당 번호의 작업이 없습니다.");
       return;
     }
@@ -128,19 +142,41 @@ public class TaskHandler {
       return;
     }
 
-    for (int i = index + 1; i < this.size; i++) {
-      this.tasks[i - 1] = this.tasks[i];
+    Node node = head;
+    Node prev = null;
+
+    while (node != null) {
+
     }
-    this.tasks[--this.size] = null;
+
+
+
+
+    prev.next = node.next;
+    node.next = null;
+
+
+
+
+
+
+
+
+
+
 
     System.out.println("작업를 삭제하였습니다.");
   }
 
   private Task findByNo(int no) {
-    for (int i = 0; i < this.size; i++) {
-      if (this.tasks[i].no == no) {
-        return this.tasks[i];
+
+    Node node = head;
+
+    while (node != null) {
+      if (node.task.no == no) {
+        return node.task;
       }
+      node = node.next;
     }
     return null;
   }
