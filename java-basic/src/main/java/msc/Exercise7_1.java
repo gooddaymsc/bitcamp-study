0a5fe2b8fb23880b846b25d131aa076057793cae
unknown <gooddaymsc@naver.com>
2021-08-18 21:20:38 +0900

    ..

--- java-basic/src/main/java/msc/Exercise7_1.java
+++ java-basic/src/main/java/msc/Exercise7_1.java
@@ -3,23 +3,54 @@ package msc;
 class SutdaDeck {
   final int CARD_NUM = 20;
   SutdaCard[] cards = new SutdaCard[CARD_NUM];
+
+
   SutdaDeck() {
 
+    for (int i = 0; i < cards.length; i++) {
 
+      int num = i % 10 + 1;
+      boolean isKwang = i < 10 && (num == 1 || num == 3 || num == 8);
 
+      cards[i] = new SutdaCard(num, isKwang);
+    }
+  }
 
+  void shuffle() {
+    for (int i = 0; i < cards.length; i++) {
+      int j = (int) (Math.random()*cards.length);
+      SutdaCard temp = cards[i];
+      cards[i] = cards[j];
+      cards[j] = temp;
+    }
+  }
+
+  SutdaCard pick(int index) {
+    if (index < 0 || index >= CARD_NUM) {
+      return null;
+    }
+    return cards[index];
+  }
+
+  SutdaCard pick() {
+    int index = (int) (Math.random()*cards.length);
+    return pick(index);
   }
 }
+
 class SutdaCard {
   int num;
   boolean isKwang;
+
   SutdaCard() {
     this(1, true);
   }
+
   SutdaCard(int num, boolean isKwang) {
     this.num = num;
     this.isKwang = isKwang;
   }
+
   // info()대신 Object클래스의 toString()을 오버라이딩했다.
   @Override
   public String toString() {
@@ -30,7 +61,12 @@ class SutdaCard {
 public class Exercise7_1 {
   public static void main(String args[]) {
     SutdaDeck deck = new SutdaDeck();
-    for(int i=0; i < deck.cards.length;i++)
+    System.out.println(deck.pick(0));
+    System.out.println(deck.pick());
+    deck.shuffle();
+    for(int i = 0; i < deck.cards.length; i++)
       System.out.print(deck.cards[i]+",");
+    System.out.println();
+    System.out.println(deck.pick(0));
   }
 }
